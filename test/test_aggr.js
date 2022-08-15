b4bf0fa80c437959c7a0c8ee88458decf76a3798
Richard Wesley <hawkfish@electricfish.com>
2022-08-15 16:03:45 -0700

    Issue #4: Test Count Aggr
    Add test and check SQL semantics.

--- test/test_aggr.js
+++ test/test_aggr.js
@@ -3,6 +3,8 @@ const aggr = require('../src/aggr');
 const expr = require('../src/expr');
 
 const CountStar = aggr.CountStar;
+const Count = aggr.Count;
+describe('Aggr', function() {
   describe('CountStar', function() {
     describe('constructor', function() {
       it('should store a zero', function() {
@@ -27,3 +29,48 @@ const CountStar = aggr.CountStar;
       });
     });
   });
+  describe('Count', function() {
+    describe('constructor', function() {
+      it('should store a zero', function() {
+          const e = new Count();
+          expect(e.count).to.equal(0);
+      });
+    });
+    describe('update', function() {
+      it('should change to one for a non-null value', function() {
+          const e = new Count();
+          e.update(1);
+          expect(e.count).to.equal(1);
+      });
+      it('should stay zero for a null value', function() {
+          const e = new Count();
+          e.update(null);
+          expect(e.count).to.equal(0);
+      });
+      it('should stay the same for a null value', function() {
+          const e = new Count();
+          e.update(1);
+          expect(e.count).to.equal(1);
+          e.update(null);
+          expect(e.count).to.equal(1);
+      });
+    });
+    describe('finalize', function() {
+      it('should be zero for no values', function() {
+          const e = new Count();
+          expect(e.finalize()).to.equal(0);
+      });
+      it('should be zero for no non-null values', function() {
+          const e = new Count();
+          e.update(null);
+          expect(e.finalize()).to.equal(0);
+      });
+      it('should stay the same for a null value', function() {
+          const e = new Count();
+          e.update(1);
+          e.update(null);
+          expect(e.finalize()).to.equal(1);
+      });
+    });
+  });
+});
