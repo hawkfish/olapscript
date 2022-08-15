f8f8811f98ba6864d9cb0e6412a2909250ed0ebb
Richard Wesley <hawkfish@electricfish.com>
2022-08-15 10:15:38 -0700

    Issue #5: Create Modules
    Break the main file up into component modules.
    Start adding tests.

---
+++ modules/aggr.js
@@ -0,0 +1,90 @@
+/**
+ * This module implements the aggregate function library for OLAPScript.
+ *
+ * Copyright © 2022 Richard Wesley and Ellen Ratajak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * The COUNTSTAR aggregate function.
+ * This also works as a single value state base class
+ *
+ */
+class CountStar {
+  constructor() {
+    this.count = 0;
+  }
+
+  update() {
+    ++this.count;
+  }
+
+  finalize() {
+    return this.count;
+  }
+};
+
+/**
+ * The COUNT aggregate function
+ *
+ */
+class Count extends CountStar {
+  update(val) {
+    this.count += (val !== null);
+  }
+};
+
+/**
+ * The SUM aggregate function
+ *
+ */
+class Sum extends Count {
+  constructor() {
+    this.sum = null;
+  }
+};
+
+Sum.prototype.update = function(val) {
+  Count.prototype.update(val);
+  if (val !== null) {
+    if (this.sum == null) {
+      this.sum = val;
+    } else {
+      this.sum += val;
+    }
+  }
+}
+
+/**
+ * The AVG aggregate function
+ *
+ */
+class Avg extends Sum {
+  finalize() {
+    if (this.count) {
+      return super.finalize() / this.count;
+    } else {
+      return null;
+    }
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports  = {
+    CountStar, Count, Sum, Avg
+  };
+};
---
+++ test/test_column.js
@@ -0,0 +1,41 @@
+const expect = require ("chai").expect;
+const column = require('../modules/column');
+
+const Column = column.Column;
+
+describe('Column', function() {
+  const count = 5;
+  const empty = Array(count).fill(null);
+  const numbers = empty.map((val, i) => i);
+  const strings = numbers.map(val => String(val));
+
+  describe('Constructor', function() {
+    it('should default to undefined/empty', function() {
+        const col = new Column();
+        expect(col.type).to.equal('undefined');
+        expect(col.data).to.be.an('array').lengthOf(0);
+    });
+    it('should store the type and data for a Number array', function() {
+        const type = typeof numbers[0];
+        const col = new Column(undefined, numbers);
+        expect(col.type).to.equal(type);
+        expect(col.data).to.equal(numbers);
+    });
+    it('should store the type and data for a String array', function() {
+        const type = typeof strings[0];
+        const col = new Column(undefined, strings);
+        expect(col.type).to.equal(type);
+        expect(col.data).to.equal(strings);
+    });
+  });
+  describe('getRowCount', function() {
+    it('should return 0 for the default constructor', function() {
+        const col = new Column();
+        expect(col.getRowCount()).to.equal(0);
+    });
+    it('should the non-empty row count', function() {
+        const col = new Column(undefined, numbers);
+        expect(col.getRowCount()).to.equal(count);
+    });
+  });
+});
