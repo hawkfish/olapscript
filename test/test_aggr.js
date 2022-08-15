17a8e4e0e160f50ed004256bdcfefd9143f1844b
Richard Wesley <hawkfish@electricfish.com>
2022-08-15 14:00:52 -0700

    Issue #4: Test CountStar
    Aggr test suite.

---
+++ test/test_aggr.js
@@ -0,0 +1,29 @@
+const expect = require ("chai").expect;
+const aggr = require('../modules/aggr');
+const expr = require('../modules/expr');
+
+const CountStar = aggr.CountStar;
+  describe('CountStar', function() {
+    describe('constructor', function() {
+      it('should store a zero', function() {
+          const e = new CountStar();
+          expect(e.count).to.equal(0);
+      });
+    });
+    describe('update', function() {
+      it('should add one', function() {
+          const e = new CountStar();
+          expect(e.count).to.equal(0);
+          e.update();
+          expect(e.count).to.equal(1);
+      });
+    });
+    describe('finalize', function() {
+      it('should add one', function() {
+          const e = new CountStar();
+          expect(e.count).to.equal(0);
+          e.update();
+          expect(e.finalize()).to.equal(1);
+      });
+    });
+  });
